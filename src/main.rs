fdc974045b2a4d223847a3ad4cc36649627d281d
TheRealSleeper <asleeper135@gmail.com>
2024-09-25 23:18:48 -0500

    Creating struct to keep track of string encoding as read from file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,60 @@
+// use regex;
+use std::{
+    env::args, error::Error, fs::{read, read_dir, read_to_string}
+};
+
+#[derive(Clone, Copy)]
+enum StrEncoding {
+    Utf8,
+    Utf16
+}
+
+struct StrUtf816 {
+    str: String,
+    encoding_type: StrEncoding
+}
+
+impl StrUtf816 {
+    pub fn as_str<'a>(&'a self) -> &'a str {
+        self.str.as_str()
+    }
+    
+    pub fn encoding(&self) -> StrEncoding {
+        self.encoding_type
+    }
+    
+    pub fn set_encoding(&mut self, value: StrEncoding) {
+        self.encoding_type = value; 
+    }
+}
+
+impl From<StrUtf816> for String {
+    fn from(value: StrUtf816) -> String {
+        value.str
+    }
+}
+
+impl AsRef<str> for StrUtf816 {
+    fn as_ref(&self) -> &str {
+        &self.as_str()
+    }
+}
+
+impl From<StrUtf816> for Vec<u8> {
+    fn from(value: StrUtf816) -> Vec<u8> {
+        match value.encoding() {
+            StrEncoding::Utf8 => {
+                String::from(value).bytes().collect()
+            }, 
+            StrEncoding::Utf16 => {
+                String::from(value).encode_utf16().flat_map(|x| x.to_ne_bytes()).collect()
+            }
+        }
+    }
+}
+
 fn main() {
-    let path = std::env::args()
+    let path = args()
         .nth(1)
         .expect("Expected 1 argument, none were given");
 
@@ -10,8 +65,8 @@ fn main() {
     }
 }
 
-fn print_dirs(path: String) -> Result<(), Box<dyn std::error::Error>> {
-    let dir = std::fs::read_dir(path);
+fn print_dirs(path: String) -> Result<(), Box<dyn Error>> {
+    let dir = read_dir(path);
     match dir {
         Ok(d) => {
             for i in d {
@@ -25,7 +80,7 @@ fn print_dirs(path: String) -> Result<(), Box<dyn std::error::Error>> {
                             println!(
                                 "{}",
                                 read_utf8_utf16(
-                                    f.path().as_os_str().to_str().unwrap().to_string()
+                                    f.path().as_os_str().to_str().unwrap()
                                 )?
                             );
                         } else if f.path().is_dir() {
@@ -42,12 +97,12 @@ fn print_dirs(path: String) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn read_utf8_utf16(path: String) -> Result<String, std::io::Error> {
-    let content = std::fs::read_to_string(path.as_str());
+fn read_utf8_utf16(path: &str) -> Result<String, std::io::Error> {
+    let content = read_to_string(path);
     match content {
         Ok(c) => Ok(c),
         Err(_) => {
-            let bytes = std::fs::read(path.as_str())?;
+            let bytes = read(path)?;
             Ok(String::from_utf16_lossy(
                 bytes
                     .chunks_exact(2)
